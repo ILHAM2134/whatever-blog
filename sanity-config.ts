0e28c49319ef41b76b2d9da786bd27a65c1b4086
ilham2134 <vicase312@gmail.com>
2023-07-01 06:30:34 +0700

    t

--- sanity-config.ts
+++ sanity-config.ts
@@ -1,22 +1,15 @@
 import { createClient } from "next-sanity";
 
-// const env = process.env.NODE_ENV;
-var Client;
-
-// if (env == "development") {
-//  Client = createClient({
-//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//     dataset: process.env.NEXT_PUBLIC_DATASET,
-//     apiVersion: "2023-06-25",
-//     useCdn: false,
-//   });
-// } else if (env == "production") {
-Client = createClient({
+// const Client = createClient({
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   dataset: process.env.NEXT_PUBLIC_DATASET,
+//   apiVersion: "2023-06-25",
+//   useCdn: false,
+// })
+const Client = createClient({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
   apiVersion: "2023-06-25",
   useCdn: false,
 });
-// }
-
 export default Client;
